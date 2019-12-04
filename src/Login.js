dec4d2e06a396a9ed0768d9243bb1cd60ed09d29
olazaida <alosha.sugar@hotmail.com>
2019-12-04 20:03:55 +0200

    semantic ui for log in

---
+++ src/Login.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment,
+} from 'semantic-ui-react';
+
+export default () => (
+  <Grid centered columns={2}>
+    <Grid.Column>
+      <Header as="h2" textAlign="center">
+        Login
+      </Header>
+      <Segment>
+        <Form size="large">
+          <Form.Input
+            fluid
+            icon="user"
+            iconPosition="left"
+            placeholder="Email address"
+          />
+          <Form.Input
+            fluid
+            icon="lock"
+            iconPosition="left"
+            placeholder="Password"
+            type="password"
+          />
+          <Button color="blue" fluid size="large">
+            Login
+          </Button>
+        </Form>
+      </Segment>
+      <Message>
+        Not registered yet? <a href="#">Sign Up</a>
+      </Message>
+    </Grid.Column>
+  </Grid>
+);
